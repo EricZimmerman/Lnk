89632d4c8b176c905839f0b4d15a8321be719c14
EricZimmerman <saericzimmerman@gmail.com>
2019-01-15 08:28:28 -0500

    remove extra return

--- Lnk/ShellItems/ShellBag0x31.cs
+++ Lnk/ShellItems/ShellBag0x31.cs
@@ -41,8 +41,6 @@ namespace Lnk.ShellItems
                         {
                             return;
                         }
-
-                        return;
                     }
                     catch (Exception)
                     {
