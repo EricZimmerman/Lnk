af7e88c18dda38189c8e3f636ab479dcc48c761c
Still Hsu <dev@stillu.cc>
2021-07-28 10:35:47 +0800

    Add an example program
    This could be used as an out-of-the-box CLI to quickly parse LNK files into JSON outputs.

---
+++ Lnk.Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Lnk.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed((options =>
+                {
+                    var lnkFile = options.InputFileInfo;
+                    var lnk = Lnk.LoadFile(lnkFile.FullName);
+                    var serialized = System.Text.Json.JsonSerializer.Serialize<LnkFile>(lnk);
+                    Console.WriteLine(serialized);
+                }));
+        }
+
+
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "The target to be parsed.")]
+            public FileInfo InputFileInfo { get; set; }
+        }
+    }
+}
